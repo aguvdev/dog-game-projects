c3fb52d980510c235baca019782f597644631f7d
aguvdev <hannheitiragustin@hotmail.com>
2023-04-04 14:08:19 -0300

    add: project 2 archivos inciales

---
+++ Project_2_parallax_bg/script.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById('canvas1');// Selecciona el elemento del HTML con el id "canvas1" y lo asigna a la constante "canvas".
+const ctx = canvas.getContext('2d');// Obtiene el contexto 2D del canvas seleccionado y lo asigna a la constante "ctx" (context).
+const CANVAS_WIDTH = canvas.width = 800;// Establece el ancho del canvas en 800 píxeles y lo asigna a la constante "CANVAS_WIDTH".
+const CANVAS_HEIGHT = canvas.height = 700;// Establece la altura del canvas en 700 píxeles y lo asigna a la constante "CANVAS_HEIGHT".
