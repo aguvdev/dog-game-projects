3fbe6cb1e67039b76b7a04277b7d5077392fd5d1
aguvdev <hannheitiragustin@hotmail.com>
2023-04-11 18:25:56 -0300

    update: movement patter 2 done

--- Project_3_enemy_movement_pattern/scriptMovement2.js
+++ Project_3_enemy_movement_pattern/scriptMovement2.js
@@ -10,44 +10,86 @@ const enemiesArray = [];
 let gameFrame = 0;
 
 class Enemy {
-  constructor(){
+  constructor() {
+    // Cargar imagen del enemigo
     this.image = new Image();
-    this.image.src = 'enemy2.png'
+    this.image.src = "enemy2.png";
+    // Velocidad aleatoria del enemigo
     this.speed = Math.random() * 4 + 1;
+    // Tamaño de la imagen de sprite
     this.spriteWidth = 266;
     this.spriteHeight = 188;
-    this.width = this.spriteWidth / 2.5;
-    this.height = this.spriteHeight / 2.5;
+    // Tamaño del enemigo
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
+    // Posición inicial aleatoria del enemigo en la pantalla
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
+    // Frame actual del sprite del enemigo
     this.frame = 0;
+    // Velocidad de animación del sprite
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    // Ángulo actual del enemigo
+    this.angle = 0;
+    // Velocidad de rotación del enemigo
+    this.angleSpeed = Math.random() * 0.2;
+    // Curva de movimiento del enemigo
+    this.curve = Math.random() * 5;
   }
-  update(){
+  update() {
+    // Mover al enemigo hacia la izquierda
     this.x -= this.speed;
-    this.y += Math.random() * 3 - 1.5;
-    if (this.x + this.width < 0) this.x = canvas.width; 
-    // animate sprites
-    if (gameFrame % this.flapSpeed === 0){
-      this.frame > 4 ? this.frame = 0 : this.frame++;
-    };
+    // Mover al enemigo en una curva sinusoidal
+    this.y += this.curve * Math.sin(this.angle);
+    // Ajustar el ángulo del enemigo
+    this.angle += this.angleSpeed;
+    // Si el enemigo sale de la pantalla, volverlo a colocar en el lado derecho
+    if (this.x + this.width < 0) this.x = canvas.width;
+    // Animar el sprite del enemigo
+    if (gameFrame % this.flapSpeed === 0) {
+      // Cambiar al siguiente frame del sprite
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
-  draw(){
-    ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+  draw() {
+    // Dibujar el sprite del enemigo en la posición actual
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
+// Creación de un array vacío para almacenar los enemigos
+// Se crea un bucle for para generar los enemigos y agregarlos al array
 for (let i = 0; i < numberOfEnemies; i++) {
   enemiesArray.push(new Enemy());
 }
 
+// Función que se encarga de la animación
 const animate = () => {
+  // Se limpia el canvas
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemiesArray.forEach(enemy => {
+
+  // Se actualizan y dibujan los enemigos del array
+  enemiesArray.forEach((enemy) => {
     enemy.update();
     enemy.draw();
   });
+
+  // Se incrementa el contador de frames del juego
   gameFrame++;
+
+  // Se llama a la función animate() de manera recursiva usando requestAnimationFrame
   requestAnimationFrame(animate);
 };
+
+// Se llama a la función animate() por primera vez para iniciar la animación
 animate();
